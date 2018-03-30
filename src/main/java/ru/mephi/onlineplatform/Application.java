0e0bce7de44d0db017cd196ae3c776e8e2040887
Stanislav_Semichevskiy <stanislav_semichevskiy@epam.com>
2018-03-30 15:21:38 +0300

    Initial commit

---
+++ src/main/java/ru/mephi/onlineplatform/Application.java
@@ -0,0 +1,11 @@
+package ru.mephi.onlineplatform;
+
+/**
+ * @author Stanislav_Semichevskiy
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        //TODO: implement main application logic here
+    }
+}
---
+++ src/test/java/ru/mephi/onlineplatform/ApplicationTest.java
@@ -0,0 +1,9 @@
+package ru.mephi.onlineplatform;
+
+/**
+ * @author Stanislav_Semichevskiy
+ */
+public class ApplicationTest {
+
+    //TODO: tests should be written here.
+}
